75154e1226912106fecfe8cd86868b62d71560b6
rans0000 <rans0000@gmail.com>
2025-05-04 15:53:31 +0530

    3d-tut: setup basic player movement

---
+++ godot-3dtut01/scripts/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody3D
+
+
+@export var base_speed := 4.0
+var movement_input:= Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	movement_input = Input.get_vector("left", "right", "forward", "backward")
+	velocity = Vector3(movement_input.x, 0, movement_input.y) * base_speed
+	move_and_slide()
