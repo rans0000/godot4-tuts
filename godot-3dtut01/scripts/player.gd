7d07b06fa4eb8abe1ce0803f00c3215257ad6263
rans0000 <rans0000@gmail.com>
2025-05-04 17:08:02 +0530

    3d-tut: set the horizontal movement of the player to align with the camera rotation

--- godot-3dtut01/scripts/player.gd
+++ godot-3dtut01/scripts/player.gd
@@ -2,9 +2,10 @@ extends CharacterBody3D
 
 
 @export var base_speed := 4.0
+@onready var camera = $CameraController/Camera3D
 var movement_input:= Vector2.ZERO
 
 func _physics_process(_delta: float) -> void:
-	movement_input = Input.get_vector("left", "right", "forward", "backward")
+	movement_input = Input.get_vector("left", "right", "forward", "backward").rotated(-camera.global_rotation.y)
 	velocity = Vector3(movement_input.x, 0, movement_input.y) * base_speed
 	move_and_slide()
