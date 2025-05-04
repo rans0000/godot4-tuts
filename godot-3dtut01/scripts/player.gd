f71b71a7f7410626f5e7e98f0fbba7d219bf4b83
rans0000 <rans0000@gmail.com>
2025-05-04 20:26:16 +0530

    3d-tut: add sprint ability to the player

--- godot-3dtut01/scripts/player.gd
+++ godot-3dtut01/scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 @export var jump_time_to_peak := 0.4
 @export var jump_time_to_descent := 0.3
 @export var base_speed := 8.0
+@export var run_speed := 24.0
 @export var acceleration := 2.0
 @export var deceleration := 4.0
 
@@ -22,12 +23,12 @@ func _physics_process(delta: float) -> void:
 
 func actor_move(delta:float) -> void:
 	movement_input = Input.get_vector("left", "right", "forward", "backward").rotated(-camera.global_rotation.y)
-	#velocity = Vector3(movement_input.x, 0, movement_input.y) * base_speed
 	var vel_2d = Vector2(velocity.x, velocity.z)
 	
 	if movement_input != Vector2.ZERO:
-		vel_2d += movement_input * base_speed * acceleration * delta
-		vel_2d = vel_2d.limit_length(base_speed)
+		var max_speed = run_speed if Input.is_action_pressed("run") else  base_speed
+		vel_2d += movement_input * max_speed * acceleration * delta
+		vel_2d = vel_2d.limit_length(max_speed)
 	else:
 		vel_2d = vel_2d.move_toward(Vector2.ZERO, base_speed * deceleration * delta)
 		
