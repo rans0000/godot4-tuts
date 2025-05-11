ebaa00aaeeda27471a917a672bc80e142dd572e3
rans0000 <rans0000@gmail.com>
2025-05-11 20:33:03 +0530

    3d-tut: add attacking animation for the boss

--- godot-3dtut01/scripts/boss.gd
+++ godot-3dtut01/scripts/boss.gd
@@ -1,4 +1,29 @@
 extends Enemy
 
+const simple_attacks = {
+	"slice": "2H_Melee_Attack_Slice",
+	"spin": "2H_Melee_Attack_Spin",
+	"range": "1H_Melee_Attack_Stab",
+}
+
 func _physics_process(delta: float) -> void:
 	move_to_player(delta)
+
+
+func _on_attack_timer_timeout() -> void:
+	var distance = position.distance_to(player.position)
+	if distance < melee_attack_radius:
+		meele_attack_animation()
+	elif distance < range_attack_radius:
+		range_attack_animation()
+
+
+func range_attack_animation() -> void:
+	stop_movement(1.5, 1.5)
+	attack_animation.animation = simple_attacks["range"]
+	$AnimationTree.set("parameters/attack_oneshot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+
+func meele_attack_animation() -> void:
+	attack_animation.animation = simple_attacks["slice" if rng.randi() % 2 else "spin"]
+	$AnimationTree.set("parameters/attack_oneshot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
--- godot-3dtut01/scripts/enemy.gd
+++ godot-3dtut01/scripts/enemy.gd
@@ -3,25 +3,37 @@ class_name Enemy
 
 @export var walk_speed := 2.0
 @export var notice_radius := 30.0
-@export var attack_radius := 15.0
+@export var chase_radius := 15.0
+@export var melee_attack_radius := 4.0
+@export var range_attack_radius := 8.0
 @onready var player:CharacterBody3D = get_tree().get_first_node_in_group("Player")
 @onready var move_state_machine:AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/move_state_machine/playback")
 @onready var skin = get_node("Skin")
-var turnaround_speed := 6.0
+@onready var attack_animation:AnimationNodeAnimation = $AnimationTree.get_tree_root().get_node("attack_animation")
+var rng = RandomNumberGenerator.new()
+const turnaround_speed := 6.0
+var speed_modifier := 1.0
 
 
 func move_to_player(delta: float) -> void:
-	if position.distance_to(player.position) > notice_radius: return
+	var distance = position.distance_to(player.position);
+	if distance > notice_radius: return
 	
 	var target_direction:Vector3 = (player.position - position).normalized()
 	var target_vec2 = Vector2(target_direction.x, target_direction.z)
 	var target_angle = -target_vec2.angle() + PI/2
 	rotation.y = rotate_toward(rotation.y, target_angle, delta * turnaround_speed)
 	
-	if position.distance_to(player.position) < attack_radius:
-		velocity = Vector3(target_vec2.x, 0, target_vec2.y) * walk_speed
+	if distance < chase_radius:
+		velocity = Vector3(target_vec2.x, 0, target_vec2.y) * walk_speed * speed_modifier
 		move_state_machine.travel("Walk")
 	else:
 		move_state_machine.travel("Idle")
 		velocity = Vector3.ZERO
 	move_and_slide()
+
+
+func stop_movement(start_duration: float, end_duration: float) -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "speed_modifier", 0.0, start_duration)
+	tween.tween_property(self, "speed_modifier", 1.0, end_duration)
